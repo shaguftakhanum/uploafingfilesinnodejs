25c6b7c35596d79b038a29be9f54c9b67a497e7d
shugufta khanum <shaguftakahnum900@gmail.com>
2022-08-02 06:10:04 -0700

    updated

--- controllers/blog.controllers.js
+++ controllers/blog.controllers.js
@@ -1,5 +1,31 @@
 const Blog = require('../model/blog');
 const File = require('../model/file');
+const createblog = async (req, res) => {
+  try {
+
+
+    const { name, images } = req.body
+
+    const blog = await Blog.create({
+      name: name
+    })
+
+    const imagesData = images.map((image) => {
+      return {
+        name: image,
+        type: 1,
+        type_id: blog.id
+
+
+      }
+    })
+
+    File.bulkCreate(imagesData);
+    res.json(blog)
+
+  } catch (error) {
+    res.send(error.message, 500);
+  }}
 
 const deleteblog = async (req, res) => {
   try {
@@ -51,6 +77,7 @@ const findoneblog = async (req, res) => {
   }
 }
 module.exports = {
+  createblog,
   deleteblog,
   findallblog,
   updatebyid,
--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -1,13 +1,15 @@
 const express=require("express");
 const router=express.Router();
-const {
 
+const {
+    createblog,
     deleteblog,
     findallblog,
     updatebyid,
     findoneblog
 
 }=require("../controllers/blog.controllers");
+router.route("/blogs/create").post(createblog);
 router.route("/blogs/:id").delete(deleteblog);
 router.route("/blogs/getall").get(findallblog);
 router.route("/blogs/:id").get(findoneblog);
